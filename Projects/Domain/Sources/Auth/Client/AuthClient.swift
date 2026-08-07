5b94fd945b2ecd0846247ec1667c18d46abbb092
gnoes.ios <park_juseong@naver.com>
2026-08-08 00:39:07 +0900

    chore: AuthClient 세션 포트 재정의

--- Projects/Domain/Sources/Auth/Client/AuthClient.swift
+++ Projects/Domain/Sources/Auth/Client/AuthClient.swift
@@ -3,9 +3,10 @@ import ThirdParty
 
 @DependencyClient
 public struct AuthClient: Sendable {
-    public var currentUser: @Sendable () async throws -> AuthUser?
-    public var signIn: @Sendable () async throws -> AuthUser
-    public var signOut: @Sendable () async throws -> Void
+    public var restoreSession: @Sendable () async throws -> AuthSession?
+    public var login: @Sendable (AuthProvider) async throws -> AuthSession
+    public var logout: @Sendable () async throws -> Void
+    public var currentSession: @Sendable () async throws -> AuthSession?
 }
 
 extension AuthClient: TestDependencyKey {
