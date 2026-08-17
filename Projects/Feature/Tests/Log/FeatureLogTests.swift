baa7757824a96ef71aa07543e259fa5bb86d8750
Lee Inho <inhoking01@gmail.com>
2026-08-17 21:47:15 +0900

    feat: 탐색 탭 게시물 조회 및 무한스크롤 구현

--- Projects/Feature/Tests/Log/FeatureLogTests.swift
+++ Projects/Feature/Tests/Log/FeatureLogTests.swift
@@ -15,7 +15,7 @@ final class FeatureLogTests: XCTestCase {
         XCTAssertEqual(FeatureLog.operationName(fromActionName: "loginResponse"), "login")
         XCTAssertEqual(FeatureLog.operationName(fromActionName: "logoutResponse"), "logout")
         XCTAssertEqual(FeatureLog.operationName(fromActionName: "sessionRestored"), "restoreSession")
-        XCTAssertEqual(FeatureLog.operationName(fromActionName: "popularContentsResponse"), "popularContents")
+        XCTAssertEqual(FeatureLog.operationName(fromActionName: "contentsResponse"), "contents")
     }
 
     func test_메시지형식_액션_상태_화면이동_오류() {
