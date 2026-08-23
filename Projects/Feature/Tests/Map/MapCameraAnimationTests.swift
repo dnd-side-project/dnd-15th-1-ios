be06f7a88b05b4a831c0834723510a9c4f33385b
gnoes.ios <park_juseong@naver.com>
2026-08-24 05:16:20 +0900

    test: 카메라 이동 들어올림 검증

--- Projects/Feature/Tests/Map/MapCameraAnimationTests.swift
+++ Projects/Feature/Tests/Map/MapCameraAnimationTests.swift
@@ -3,8 +3,8 @@ import XCTest
 
 final class MapCameraAnimationTests: XCTestCase {
 
-    func test_카메라_이동은_들어올리지_않는다() {
-        XCTAssertFalse(MapCameraMove.autoElevation)
+    func test_카메라_이동은_들어올린다() {
+        XCTAssertTrue(MapCameraMove.autoElevation)
     }
 
     func test_카메라_이동_시간은_250밀리초다() {
