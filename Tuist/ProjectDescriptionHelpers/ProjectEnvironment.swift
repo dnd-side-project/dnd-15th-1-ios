3d7c44db9ae878ecc3508085e52c5f1945cfd384
gnoes.ios <park_juseong@naver.com>
2026-08-29 02:10:19 +0900

    chore: 릴리즈 버전을 1.0.1 로 올리고 빌드 번호를 1 로 초기화

--- Tuist/ProjectDescriptionHelpers/ProjectEnvironment.swift
+++ Tuist/ProjectDescriptionHelpers/ProjectEnvironment.swift
@@ -8,8 +8,8 @@ public enum ProjectEnvironment {
     public static let productName = "Dulpick"
     public static let displayName = "둘픽"
 
-    public static let appVersion = "1.0.0"
-    public static let appBuildNumber = "3"
+    public static let appVersion = "1.0.1"
+    public static let appBuildNumber = "1"
 
     public static let swiftVersion = "6"
     public static let deploymentTarget = "18.0"
