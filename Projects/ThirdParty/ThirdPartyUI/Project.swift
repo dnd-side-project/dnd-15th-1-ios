64d2bd59890f0110447d4507b46fc550474e1548
gnoes.ios <park_juseong@naver.com>
2026-09-08 01:00:54 +0900

    chore: 믹스패널 스위프트 SDK 6.3.0 의존 추가

--- Projects/ThirdParty/ThirdPartyUI/Project.swift
+++ Projects/ThirdParty/ThirdPartyUI/Project.swift
@@ -7,12 +7,14 @@ let project = ProjectFactory.thirdParty(
         .package(url: "https://github.com/kean/Nuke", .exact("12.9.0")),
         .package(url: "https://github.com/kakao-mapsSDK/KakaoMapsSDK-SPM", .exact("2.12.17")),
         .package(url: "https://github.com/microsoft/clarity-apps", .exact("4.0.0")),
+        .package(url: "https://github.com/mixpanel/mixpanel-swift", .exact("6.3.0")),
     ],
     productDependencies: [
         .package(product: "Nuke"),
         .package(product: "NukeUI"),
         .package(product: "KakaoMapsSDK-SPM"),
         .package(product: "Clarity"),
+        .package(product: "Mixpanel"),
     ],
     product: .framework
 )
